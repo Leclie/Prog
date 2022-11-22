167d041bb37ce70a611ce4517d68963a748f9b07
Leclie <07140714no@gmail.com>
2022-11-22 22:54:28 +0300

    first homework

---
+++ task 4_hw/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+Console.WriteLine("Введите 1е число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2е число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 3е число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3){
+    Console.WriteLine(num1);
+}
+if (num2 > num1 && num2 > num3){
+    Console.WriteLine(num2);
+}
+if (num3 > num2 && num3 > num1){
+    Console.WriteLine(num3);
+}
---
+++ task 6_hw/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0){
+    Console.WriteLine("Да");
+} else{
+    Console.WriteLine("Нет");
+}
